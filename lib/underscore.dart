d0f07520739c058e297d6f24ed37f7209edb45c6
bitinfinitywebsolutions <bitinfinitywebsolutionspvtltd@gmail.com>
2024-06-13 16:08:44 +0530

    library updated!!

--- lib/underscore.dart
+++ lib/underscore.dart
@@ -10,53 +10,53 @@ export "package:underscore/array/compact.dart";
 export "package:underscore/array/concat.dart";
 export "package:underscore/array/difference.dart";
 export "package:underscore/array/drop.dart";
-export "package:underscore/array/dropRight.dart";
+export "package:underscore/array/drop_right.dart";
 export 'package:underscore/array/remove.dart';
 export 'package:underscore/array/fill.dart';
-export 'package:underscore/array/removeAt.dart';
+export 'package:underscore/array/remove_at.dart';
 export 'package:underscore/array/join.dart';
-export 'package:underscore/array/findIndex.dart';
-export 'package:underscore/array/findLastIndex.dart';
+export 'package:underscore/array/find_index.dart';
+export 'package:underscore/array/find_last_index.dart';
 export 'package:underscore/array/pull.dart';
-export 'package:underscore/array/uniqBy.dart';
+export 'package:underscore/array/uniq_by.dart';
 export 'package:underscore/array/uniq.dart';
-export 'package:underscore/array/zipWith.dart';
-export 'package:underscore/array/zipObject.dart';
+export 'package:underscore/array/zip_with.dart';
+export 'package:underscore/array/zip_object.dart';
 export 'package:underscore/array/zip.dart';
 export 'package:underscore/array/reverse.dart';
-export 'package:underscore/array/indexOf.dart';
+export 'package:underscore/array/index_of.dart';
 export 'package:underscore/array/last.dart';
 export 'package:underscore/array/slice.dart';
 
 // TODO :: Collection
-export "package:underscore/collection/sortBy.dart";
+export "package:underscore/collection/sort_by.dart";
 export "package:underscore/collection/size.dart";
-export "package:underscore/collection/groupBy.dart";
+export "package:underscore/collection/group_by.dart";
 export "package:underscore/collection/filter.dart";
 export "package:underscore/collection/shuffle.dart";
 export "package:underscore/collection/some.dart";
 
 // TODO :: String
-export 'package:underscore/string/camelCase.dart';
+export 'package:underscore/string/camel_case.dart';
 export 'package:underscore/string/capitalize.dart';
-export 'package:underscore/string/endsWith.dart';
+export 'package:underscore/string/ends_with.dart';
 export 'package:underscore/string/trim.dart';
 export 'package:underscore/string/words.dart';
 
 // TODO :: Util
 export "package:underscore/util/property.dart";
-export "package:underscore/util/matchesProperty.dart";
+export "package:underscore/util/matches_property.dart";
 export "package:underscore/util/matches.dart";
 
 // TODO :: Lang
-export 'package:underscore/lang/isInteger.dart';
-export 'package:underscore/lang/toNumber.dart';
-export 'package:underscore/lang/toInteger.dart';
-export 'package:underscore/lang/isEqual.dart';
+export 'package:underscore/lang/is_integer.dart';
+export 'package:underscore/lang/to_number.dart';
+export 'package:underscore/lang/to_integer.dart';
+export 'package:underscore/lang/is_equal.dart';
 
 // TODO :: Number
 export 'package:underscore/number/clamp.dart';
-export 'package:underscore/number/inRange.dart';
+export 'package:underscore/number/in_range.dart';
 
 // TODO :: Math
 export 'package:underscore/math/add.dart';
@@ -67,4 +67,4 @@ export 'package:underscore/math/divide.dart';
 export "package:underscore/object/get.dart";
 export "package:underscore/object/keys.dart";
 export "package:underscore/object/values.dart";
-export "package:underscore/object/getJsonValue.dart";
+export "package:underscore/object/get_json_value.dart";
